a06ad6a727629a5b4551697694b936b415a34fbc
toxtaboyevsamandar72 <toxtaboyevsamandar720@gmail.com>
2024-02-29 22:26:11 +0500

    index.html

---
+++ js/script.js
@@ -0,0 +1,94 @@
+// let user = {
+//     name: 'Samandar',
+//     age: 20,
+//     email: "toxtaboyevsamandar720@gmail.com",
+//     location: "farg'ona",
+//     lang: ['uzbek', 'english'],
+//     login: function () {
+//         console.log('siz bu sayitni koryabsiz');
+//     },
+//     logout: function () {
+//         console.log('siz bu sahifani kormaysiz endi');
+//     },
+//     speak: function () {
+//         console.log(this.name);
+//         console.log('I can speak');
+//         this.langs.forEach((lang) => {
+//             console.log(lang);
+//         });
+//     },
+// }
+
+
+
+
+// let movies = [
+//     {name: "avatar", likes: 456}
+// ]
+
+// movies.forEach(movies => {
+//     let result = `kino nomi: ${movies.name}, likes: ${movies.likes}`
+//     console.log(result);
+// });
+
+// user.speak()
+// console.log(this);
+
+
+
+
+// user.login();
+// user.logout();
+
+
+// console.log(user);
+// console.log(user.email);
+
+// user.age = 25
+
+// console.log(user.age);
+// console.log(user["location"]);
+
+
+
+// console.log(Math.E);
+// console.log(Math.pi);
+
+// let area = 7.5
+
+// console.log(Math.round(area));
+// console.log(Math.floor(area));
+
+// console.log(Math.ceil(area));
+// console.log(Math.trunc(area));
+// console.log(Math.trunc(Math.random()*10)+1);
+
+
+
+
+// let movies = [
+//     { name: 'Avatar', year: 2004, rating: 2 },
+//     { name: 'Titanik', year: 1976, rating: 3 },
+//     { name: 'Forsaj', year: 2024, rating: 4 },
+//     { name: 'Avengers', year: 2017, rating: 6 },
+//   ]
+  
+//   let sortedMovies = movies.sort((arg1, arg2) => {
+//     return arg2.rating - arg1.rating
+//   })
+  
+//   console.log(sortedMovies)
+  
+  
+  // let names = ['ahror', 'sardor', 'doniyor', 'bobur']
+  
+  //let sortedNames = names.sort()
+  // // console.log(sortedNames)
+  
+  // // numbers
+  // let numbers = [3, 1, 19, 11, 7, 5]
+  
+  // let sortedNumbers = numbers.sort((arg1, arg2) => {
+  //   return arg2 - arg1
+  // })
+  // console.log(sortedNumbers)
